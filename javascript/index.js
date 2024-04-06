d58a36095d5bfeb367d2280b92e2cf9f7ada6a4d
tejasshri <tejasshrishekhar08@gmail.com>
2024-04-06 16:48:52 +0530

    full_biopsy_landing_page

---
+++ javascript/index.js
@@ -0,0 +1,54 @@
+const animationBoxEl = document.querySelector(".why-cion__animation-box");
+const movingEl = document.querySelectorAll(".why-cion__center-box");
+
+const centerEl = document.querySelector(".why-cion__center");
+
+let isVisible = false;
+
+function updateAnimation() {
+  if (isVisible) {
+    if (window.innerWidth <= 768) {
+      animationBoxEl.style.width = "18.81rem";
+      animationBoxEl.style.height = "12.245rem";
+      // animationBoxEl.style.visibility = "visible";
+      animationBoxEl.style.opacity = "1";
+      return;
+    }
+    animationBoxEl.style.width = "26.58490566037736rem";
+    animationBoxEl.style.height = "12.245rem";
+    // animationBoxEl.style.visibility = "visible";
+    animationBoxEl.style.opacity = "1";
+  } else {
+    animationBoxEl.style.width = "50%";
+    animationBoxEl.style.height = "50%";
+    // animationBoxEl.style.visibility = "hidden";
+    animationBoxEl.style.opacity = "0";
+  }
+}
+
+updateAnimation();
+
+
+window.addEventListener("resize", (event) => {
+  updateAnimation();
+});
+
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      isVisible = true;
+      updateAnimation();
+    } else {
+      isVisible = false;
+      updateAnimation();
+    }
+  });
+}, options);
+
+observer.observe(centerEl)
